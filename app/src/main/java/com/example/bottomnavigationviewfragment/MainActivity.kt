e6d652f8329755338b45d0276a16dbecd409df5b
Vympel87 <111189503+Vympel87@users.noreply.github.com>
2023-10-24 19:25:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigationviewfragment/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.bottomnavigationviewfragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.bottomnavigationviewfragment.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+        with(binding) {
+            replaceFragment(HomeFragment())
+            bottomNavigation.setOnItemSelectedListener {
+                when(it.itemId) {
+                    R.id.action_Home -> replaceFragment(HomeFragment())
+                    R.id.action_Notification -> replaceFragment(NotificationFragment())
+                    R.id.action_Profile -> replaceFragment(ProfileFragment())
+                }
+                true
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
+
+}
